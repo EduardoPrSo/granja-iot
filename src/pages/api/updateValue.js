d3e7e4e5cc0c75bcd9a73c5089533d4a08ddf914
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-06-12 15:57:11 -0300

    fix

--- src/pages/api/updateValue.js
+++ src/pages/api/updateValue.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const conn = await database()
     const {body} = req;
 
-    await conn.query(`UPDATE 'User_metrics_setup' SET 'value' = ${body.value} WHERE 'user_id' = 1 AND 'metric_name' = ${body.name}`)
+    await conn.query(`UPDATE User_metrics_setup SET value = '${body.value}' WHERE user_id = '1' AND metric_name = '${body.name}'`)
 
     res.status(200).json({id: body})
 }
